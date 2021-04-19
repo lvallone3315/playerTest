16d262065303d81cba53d8ce6d3770824994191d
Lee Vallone <lvallone3315@gmail.com>
2021-04-19 19:56:35 -0400

    TDD Demo - Empty class + a few JUnit tests

--- src/playertest/PlayerTest.java
+++ src/playertest/PlayerTest.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Demonstration of Test Driven Development
  */
 package playertest;
 
@@ -9,29 +7,10 @@ package playertest;
  *
  * @author Lee
  */
-public class PlayerTest {
-    
-    String playerName;
 
-    /**
-     * @param args the command line arguments
-     */
+  public class PlayerTest {
+      
     public static void main(String[] args) {
-        // TODO code application logic here
     }
-    
-    PlayerTest() {}
-    
-    PlayerTest(String name) {
-        playerName = name;
-    }
-    
-    public void setPlayerName(String name) {
-        playerName = name;
-    }
-    
-    public String getPlayerName() {
-        return playerName;
-    }
-    
-}
+  }
+
--- test/playertest/TestPlayerTest.java
+++ test/playertest/TestPlayerTest.java
@@ -40,13 +40,63 @@ public class TestPlayerTest {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
+    
+    /*
+     * Requirements
+     *   Unlimited” number of players
+     *   Name limited to 24 characters, additional characters truncated
+     *   If no name entered, default name is ?
+     *   Names - Spaces allowed
+     *   Names – Other non-printables okay, but expect newlines to be removed prior to calling
+     * Design
+     *   Player class
+     *   PlayerTest()  - constructor uninitialized
+     *   PlayerTest(String) - constructor initialized
+     *   Void setPlayerName(string) - sets player name
+     *   String getPlayerName() - returns player name
+    */
+    
+    // First test creating multiple instances & setting & retrieving names
+    //   Create instance, store Bob in 1st instance, retrieve & verify Bob returned
+    //   Create 2nd instance, store Nancy, retrieve & verify Nancy returned
     @Test
     public void playerTester() {
         String playerName = "Bob";
         PlayerTest player = new PlayerTest();
         player.setPlayerName(playerName);
-        assertEquals(playerName, player.getPlayerName());
-        PlayerTest player2 = new PlayerTest("Nancy");
-        assertEquals("Nancy", player2.getPlayerName());
+        assertEquals("Bob NOT returned", playerName, player.getPlayerName());
+    }
+    
+    // test creating multiple instances & initializing via constructor
+    //   Create Bob again
+    //   Create 2nd instance initialized to Nancy, retrieve & verify Nancy returned
+    @Test
+    public void playerTesterInitializedConstructor() {
+        String playerName = "Bob";
+        PlayerTest player = new PlayerTest();
+        player.setPlayerName(playerName);
+        assertEquals("Bob NOT returned", playerName, player.getPlayerName());
+        
+        // PlayerTest player2 = new PlayerTest("Nancy");
+        // assertEquals("Nancy NOT returned", "Nancy", player2.getPlayerName());
+    }
+    
+    // Next test names limited to 24 characters
+    //   verify 24 characters good, 25 characters truncated to 24
+    @Test
+    public void playerTester24Char() {
+        String playerName25Char = "123456789x123456789y12345";
+        String playerName24Char = "123456789x123456789y1234";
+        
+        PlayerTest player = new PlayerTest();
+        player.setPlayerName(playerName24Char);
+        assertEquals("24 character name NOT accepted", playerName24Char, player.getPlayerName());
+        
+        
+        // player.setPlayerName(playerName25Char);
+        assertNotEquals("25 character name accepted (should truncate to 24", playerName25Char, player.getPlayerName());
+        assertEquals("name different, but not truncated", playerName24Char, player.getPlayerName());
+        // 
+        
     }
 }
